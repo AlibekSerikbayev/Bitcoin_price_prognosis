fd99610ac2dcf42a6e7ef8e0476ae8a75bd83eb8
AlibekSerikbayev <aliserikbek55@gmail.com>
2024-12-03 17:28:36 +0500

    app 4

--- app.py
+++ app.py
@@ -248,7 +248,7 @@ import pandas as pd
 import joblib
 
 # Modelni yuklash
-model = joblib.load('bitcoin_model.pkl')
+model = joblib.load('bitcoin_model2.pkl')
 
 # Interfeys
 st.title("Bitcoin Narxi Bashorati")
